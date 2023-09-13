5a2639ea52610400e34b1a76f9e2c6d07431bbc1
Alex-xius <135141980+Alex-xius@users.noreply.github.com>
2023-09-13 21:00:12 +0300

    2.1 Сделал функциональные тесты

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse('''<html><title>Сайт Алексея Исаева</title>
-                        <h1>Алексей Исаев</h1>
-                        </html>''')
+                        <h1>Алексей Исаев</h1></html>''')
--- functional_tests.py
+++ functional_tests.py
@@ -7,8 +7,9 @@ class BasicInstallTest(unittest.TestCase):
     '''Я решил прокачаться в когортном анализе
     Зшаел в гугл, ввел запрос и кликнул по одной из ссылок'''
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Chrome()  
+        self.browser.get('http://localhost:8000')
 
     def tearDown(self):  
         self.browser.quit()
@@ -16,14 +17,24 @@ class BasicInstallTest(unittest.TestCase):
     def test_page_title(self):  
         # В браузере открылся сайт (по адресу http://127.0.0.1:8000)
         # В заголовке было написано 'Сайт Алексея Исаева'
-        self.browser.get('http://localhost:8000')  
         self.assertIn('Сайт Алексея Исаева', self.browser.title)  
 
     def test_page_header(self):  
         # В шапке сайта написанно "Алексей Исаев"
-        self.browser.get('http://localhost:8000')  
         header = self.browser.find_element(By.TAG_NAME, 'h1')
         self.assertIn('Алексей Исаев', header.text) 
+
+    def test_home_page_blog(self):
+        #Под шапкой расположен блок со статьями
+        article_list = self.browser.find_elements(By.CLASS_NAME, 'article-list')
+        self.assertTrue(article_list)
+
+    def test_home_page_articales_look_correct(self):
+        # У кажлй статьт есть заголовок  и 1 абзац с текстом 
+        article_title = self.browser.find_elements(By.CLASS_NAME, 'article-title')
+        article_summary = self.browser.find_elements(By.CLASS_NAME, 'article-summary')
+        self.assertTrue(article_title)
+        self.assertTrue(article_summary)
 
 
 if __name__ == '__main__':  
